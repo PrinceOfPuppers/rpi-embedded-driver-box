baa31284db9e925a9b06e5b164dd35d4ce264e57
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2022-01-26 22:40:38 -0500

    improved device finding for sense hat

--- src/sense-api.c
+++ src/sense-api.c
@@ -12,7 +12,7 @@
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 
-#define DEV_PATH "/dev/fb1"
+#define DEV_PATH_FMT "/dev/fb%c"
 
 int fd;
 
@@ -21,37 +21,42 @@ uint16_t * getLedArr(){
     uint16_t *ledArr;
     struct fb_fix_screeninfo fix_info;
 
-    // open frame buffer
-    fd = open(DEV_PATH, O_RDWR);
-    if (fd == -1) {
-        fprintf(stderr, "Error When Opening %s : ",DEV_PATH);
-	    perror("");
-	    exit(EXIT_FAILURE);
-    }
+    char * devPath = malloc(strlen(DEV_PATH_FMT));
+    for(int i = 0; i<3; i++){
+        char numChar = i + '0';
+        sprintf(devPath, DEV_PATH_FMT, numChar);
 
-    // read framebuffer info for device so we can check to see if its the correct one
-    if (ioctl(fd, FBIOGET_FSCREENINFO, &fix_info) == -1) {
-	    perror("Unable to get device info ");
-	    close(fd);
-	    exit(EXIT_FAILURE);
-    }
+        // open frame buffer
+        fd = open(devPath, O_RDWR);
+        if (fd == -1) {
+            continue;
+        }
 
-    // check the frame buffers id to ensure it matches the pi sense hat's frame buffer id
-    if (strcmp(fix_info.id, "RPi-Sense FB") != 0) {
-	    fprintf(stderr,"Error: Device id %s Found Instead of 'RPi-Sense FB'\n", fix_info.id);
-        fprintf(stderr,"Frame Buffer Length: %i Expected: %i \n",fix_info.smem_len, FILESIZE);
-	    close(fd);
-	    exit(EXIT_FAILURE);
-    }
+        // read framebuffer info for device so we can check to see if its the correct one
+        if (ioctl(fd, FBIOGET_FSCREENINFO, &fix_info) == -1) {
+            close(fd);
+            continue;
+        }
 
-    // map frame buffer into virtual memory
-    ledArr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (ledArr == MAP_FAILED) {
-	    close(fd);
-	    perror("Error mmapping the file");
-	    exit(EXIT_FAILURE);
+        // check the frame buffers id to ensure it matches the pi sense hat's frame buffer id
+        if (strcmp(fix_info.id, "RPi-Sense FB") != 0) {
+            close(fd);
+            continue;
+        }
+
+        // map frame buffer into virtual memory
+        ledArr = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (ledArr == MAP_FAILED) {
+            close(fd);
+            perror("Error mmapping the file");
+            exit(EXIT_FAILURE);
+        }
+        free(devPath);
+        return ledArr;
     }
-    return ledArr;
+    fprintf(stderr,"Error Getting Sense Hat");
+    exit(EXIT_FAILURE);
+
 }
 
 
